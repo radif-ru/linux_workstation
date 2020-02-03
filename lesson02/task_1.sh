91649d4649556a0c223805517eb2a6041a4e52a9
Radif <23radif@gmail.com>
2020-02-03 13:30:20 +0300

    lesson02 (#2)
    lesson02

---
+++ lesson02/task_1.sh
@@ -0,0 +1,10 @@
+# Задание 1.
+
+# Просмотреть содержимое директорий /etc, /proc, /home. 
+# Посмотреть пару произвольных файлов в /etc.
+
+ls /etc /proc /home -lA
+vim /etc/profile
+:!q
+vim /etc/legal
+:!q
