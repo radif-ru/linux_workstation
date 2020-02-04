1b30d10d4927db6b79828193e9a3ce3d7ad8ec0b
Radif <23radif@gmail.com>
2020-02-04 18:35:07 +0300

    task7-2var

--- lesson03/task_7.sh
+++ lesson03/task_7.sh
@@ -7,9 +7,11 @@
 # Примечание. Задания с 5 по 7 даны для тех, кому упражнений 1-4 показалось недостаточно.
 
 mkdir invisible_files;
-cd invisible_files;
 touch invis{01..05}.inv;
-for i in {01..05};
-do mv invis$i.inv .invis$i.inv;
-done;
-ls # теперь так файлов не видно
+chmod -r invisible_files; # запрет на чтение директории
+# 2 вариант решения (делаем скрытыми все файлы):
+# cd invisible_files;
+# for i in {01..05};
+# do mv invis$i.inv .invis$i.inv;
+# done;
+ls # в обоих вариантах решения так файлов не видно, но зная имя можно открыть
